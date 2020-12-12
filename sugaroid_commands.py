d1c7efa6f5827c67bc7606fb223cd0a3482c4a52
Srevin Saju <srevinsaju@sugarlabs.org>
2020-12-12 20:02:41 +0300

    Deploying from post-hook @ 24af654c032e3ef912be8d59a452e8fa851308b6 \n\n style: update sugaroid style

--- sugaroid_commands.py
+++ sugaroid_commands.py
@@ -23,33 +23,54 @@ class SugaroidDiscordCommands:
             return await self.commands.get(command, self.do_nothing)(message)
         else:
             return False
-    
+
     async def do_nothing(self, message):
         return False
 
     async def stat(self, message):
         # get and send stats
-        # Modified from 
+        # Modified from
         # https://github.com/SwagLyrics/SwagLyrics-discord-bot/blob/50d05b60c913981db80c223e57bba2c5b8c6dded/SwagLyricsBot/dev_commands.py#L23-$6
         info = await self.client.application_info()
         total_ram = (psutil.virtual_memory().total >> 30) + 1
         embed = discord.Embed(
             title="Sugaroid Stats",
             description=f"Running on a Heroku server with {total_ram}GB RAM \n.")
-        embed.add_field(name="**__General Info__**", inline=False, value="\u200b")
-        embed.add_field(name="Latency", value=f"{self.client.latency*1000:.03f}ms")
-        embed.add_field(name="Guild Count", value=f"{len(self.client.guilds):,}")
+        embed.add_field(
+            name="**__General Info__**",
+            inline=False,
+            value="\u200b")
+        embed.add_field(
+            name="Latency",
+            value=f"{self.client.latency*1000:.03f}ms")
+        embed.add_field(
+            name="Guild Count",
+            value=f"{len(self.client.guilds):,}")
         embed.add_field(name="User Count", value=f"{len(self.client.users):,}")
-        embed.add_field(name="**__Technical Info__**", inline=False, value="\u200b")
-        embed.add_field(name="System CPU Usage", value=f"{psutil.cpu_percent():.02f}%")
-        embed.add_field(name="System RAM Usage",
-                        value=f"{psutil.virtual_memory().used/1048576:.02f} MB")
-        embed.add_field(name="System Uptime",
-                        value=f'{timedelta(seconds=int(time.time() - psutil.boot_time()))}')
-        embed.add_field(name="Bot CPU Usage", value=f"{process.cpu_percent():.02f}%")
-        embed.add_field(name="Bot RAM Usage", value=f"{process.memory_info().rss / 1048576:.02f} MB")
-        embed.add_field(name="Bot Uptime",
-                        value=f'{timedelta(seconds=int(time.time() - process.create_time()))}')
-        embed.set_footer(text=f'Made by {info.owner}', icon_url=info.owner.avatar_url)
+        embed.add_field(
+            name="**__Technical Info__**",
+            inline=False,
+            value="\u200b")
+        embed.add_field(
+            name="System CPU Usage",
+            value=f"{psutil.cpu_percent():.02f}%")
+        embed.add_field(
+            name="System RAM Usage",
+            value=f"{psutil.virtual_memory().used/1048576:.02f} MB")
+        embed.add_field(
+            name="System Uptime",
+            value=f'{timedelta(seconds=int(time.time() - psutil.boot_time()))}')
+        embed.add_field(
+            name="Bot CPU Usage",
+            value=f"{process.cpu_percent():.02f}%")
+        embed.add_field(
+            name="Bot RAM Usage",
+            value=f"{process.memory_info().rss / 1048576:.02f} MB")
+        embed.add_field(
+            name="Bot Uptime",
+            value=f'{timedelta(seconds=int(time.time() - process.create_time()))}')
+        embed.set_footer(
+            text=f'Made by {info.owner}',
+            icon_url=info.owner.avatar_url)
         await message.channel.send(embed=embed)
         return True
