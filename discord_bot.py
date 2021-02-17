44eca12d96b5b1d8579a64d1be5d8730349e2043
Srevin Saju <srevinsaju@sugarlabs.org>
2021-02-17 23:44:51 +0300

    feat: arrange into packets and then format the messages to discord type

--- discord_bot.py
+++ discord_bot.py
@@ -24,60 +24,83 @@ init_cpu_time = process.cpu_percent()
 
 
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+token = os.getenv("DISCORD_TOKEN")
 sg = sugaroid.Sugaroid()
 sg.toggle_discord()
 client = discord.Client()
 interrupt_local = False
 start_time = datetime.now()
+message_length_limit = 1990
 
 
-async def update_sugaroid(message, branch='master'):
+formatters = {
+    "<b>": "**",
+    "</b>": "**",
+    "<i>": "_",
+    "</i>": "_",
+    "<pre><code>": "```",
+    "</code></pre>": "```",
+}
+
+
+def split_into_packets(response: str) -> list:
+    messages = []
+    for i in range(0, len(response), message_length_limit):
+        messages.append(response[i : i + message_length_limit])
+
+    broken_messages = []
+    for message in messages:
+        broken_messages.extend(message.split("<br>"))
+
+    return broken_messages
+
+
+def format_messages(message: str) -> str:
+    new_message = message
+    for i in formatters:
+        new_message = new_message.replace(i, formatters[i])
+
+
+async def update_sugaroid(message, branch="master"):
     # initiate and announce to the user of the upgrade
-    await message.channel.send(
-        "Updating my brain with new features :smile:"
-    )
+    await message.channel.send("Updating my brain with new features :smile:")
 
     # execute pip3 install
-    pip = shutil.which('pip')
+    pip = shutil.which("pip")
     pip_popen_subprocess = subprocess.Popen(
         shlex.split(
-            f'{pip} install --upgrade --force-reinstall --no-deps '
-            f'https://github.com/srevinsaju/sugaroid/archive/{branch}.zip'
+            f"{pip} install --upgrade --force-reinstall --no-deps "
+            f"https://github.com/srevinsaju/sugaroid/archive/{branch}.zip"
         ),
         stdout=sys.stdout,
-        stderr=sys.stderr
+        stderr=sys.stderr,
     )
     # reload modules
-    os.chdir('/')
+    os.chdir("/")
     importlib.reload(sug)
     importlib.reload(sugaroid)
     importlib.reload(version)
 
     # updating the bot
     os.chdir(os.path.dirname(sug.__file__))
-    git = shutil.which('git')
+    git = shutil.which("git")
     # reset --hard
     git_reset_popen_subprocess = subprocess.Popen(
-        shlex.split(f'{git} reset --hard origin/master'),
+        shlex.split(f"{git} reset --hard origin/master"),
         stdout=sys.stdout,
-        stderr=sys.stderr
+        stderr=sys.stderr,
     ).wait(500)
     # git pull
     git_pull_popen_subprocess = subprocess.Popen(
-        shlex.split(f'{git} pull'),
-        stdout=sys.stdout,
-        stderr=sys.stderr
+        shlex.split(f"{git} pull"), stdout=sys.stdout, stderr=sys.stderr
     )
 
     importlib.reload(scom)
 
     await client.change_presence(
         activity=discord.Game(
-            name='v{} since {:02d}:{:02d} UTC'.format(
-                version.VERSION,
-                datetime.utcnow().hour,
-                datetime.utcnow().minute
+            name="v{} since {:02d}:{:02d} UTC".format(
+                version.VERSION, datetime.utcnow().hour, datetime.utcnow().minute
             )
         )
     )
@@ -88,43 +111,53 @@ async def update_sugaroid(message, branch='master'):
 
 @client.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
+    print(f"{client.user.name} has connected to Discord!")
     os.chdir(os.path.dirname(sug.__file__))
-    await client.change_presence(activity=discord.Game(name='v{} since {:02d}:{:02d} UTC'
-                                                       .format(version.VERSION, datetime.utcnow().hour,
-                                                               datetime.utcnow().minute)))
+    await client.change_presence(
+        activity=discord.Game(
+            name="v{} since {:02d}:{:02d} UTC".format(
+                version.VERSION, datetime.utcnow().hour, datetime.utcnow().minute
+            )
+        )
+    )
 
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
-        print("Ignoring message sent by another Sugaroid Instance")
+        # print("Ignoring message sent by another Sugaroid Instance")
+        return
+    if isinstance(message.channel, discord.channel.DMChannel):
+        # ban private messages to sugaroid
         return
     global interrupt_local
 
-    if any((
-        message.content.startswith('<@684746563540484099>'),
-        message.content.startswith('<@!684746563540484099>'),
-        message.content.startswith('!S')
-    )):
+    if any(
+        (
+            message.content.startswith(f"<@{client.user.id}>"),
+            message.content.startswith(f"<@!{client.user.id}>"),
+            message.content.startswith("!S"),
+        )
+    ):
         # make the user aware that Sugaroid received the message
         async with message.channel.typing():
             # clean the message
-            msg = message.content\
-                .replace('<@684746563540484099>', '')\
-                .replace('<@!684746563540484099>', '')\
-                .replace('!S', '')\
+            msg = (
+                message.content.replace(f"<@{client.user.id}>", "")
+                .replace(f"<@!{client.user.id}>", "")
+                .replace("!S", "")
                 .strip()
+            )
 
             command_processor = scom.SugaroidDiscordCommands(client)
 
             is_valid_command = await command_processor.call_command(msg, message)
-            print("Recv", is_valid_command)
+            # print("Recv", is_valid_command)
             if is_valid_command:
                 return
 
-            elif 'update' in msg and len(msg) <= 7:
-                if str(message.author) == 'srevinsaju#8324':
+            elif "update" in msg and len(msg) <= 7:
+                if str(message.author) == "srevinsaju#8324":
                     parts = msg.split()[-1]
                     if parts.lower() == "update":
                         parts = "master"
@@ -137,8 +170,8 @@ async def on_message(message):
                     )
                 return
 
-            elif 'stop' in message.content and 'learn' in message.content:
-                if str(message.author) == 'srevinsaju#8324':
+            elif "stop" in message.content and "learn" in message.content:
+                if str(message.author) == "srevinsaju#8324":
                     global interrupt_local
                     interrupt_local = False
                     await message.channel.send("InterruptAdapter terminated")
@@ -148,57 +181,43 @@ async def on_message(message):
                         f"Seems like you do not have sufficient permissions"
                     )
                 return
-            lim = 1995
             try:
                 response = sg.parse(msg)
-            except Exception as e:
+            except Exception:
                 # some random error occured. Log it
                 error_message = traceback.format_exc(chain=True)
-                response = "```An unhandled exception occurred: " + error_message + "```"
-
-            if len(str(response)) >= lim:
-                response1 = str(response)[:lim] + '...'
-                await message.channel.send(response1)
-                if len(str(response)) >= (2 * lim):
-                    response2 = str(response)[lim:2 * lim] + '...'
-                    await message.channel.send(response2)
-
-                    if len(str(response)) >= (3 * lim):
-                        response2 = str(response)[2 * lim:3 * lim] + '...'
-                        await message.channel.send(response2)
-                        response2 = str(response)[3 * lim:4 * lim]
-                        await message.channel.send(response2)
-                    else:
-                        response2 = str(response)[2 * lim:3 * lim]
-                        await message.channel.send(response2)
-                else:
-                    response2 = str(response)[lim:2 * lim]
-                    await message.channel.send(response2)
-            else:
-                await message.channel.send(response)
-                return
+                response = (
+                    "```py\nAn unhandled exception occurred: " + error_message + "```"
+                )
+            print(f"sugaroid: {response}")
+            for packet in split_into_packets(str(response)):
+                if packet:
+                    # only try to send if the message is not empty
+                    await message.channel.send(format_messages(packet))
+            return
         return
 
     elif interrupt_local:
         token = word_tokenize(message.content)
         for i in range(len(token)):
-            if str(token[i]).startswith('@'):
+            if str(token[i]).startswith("@"):
                 token.pop(i)
         if len(token) <= 5:
-            messages = ' '.join(token)
+            messages = " ".join(token)
             author = message.author.mention
             sg.append_author(author)
             sg.interrupt_ds()
             response = sg.parse(messages)
-            print(response, 's' * 5)
-            await message.channel.send(response)
+            # print(response, "s" * 5)
+            await message.channel.send(format_messages(response))
         return
 
 
 @client.event
 async def on_member_join(member):
     for channel in member.server.channels:
-        if channel.name == 'general':
-            await channel.send(channel, 'Welcome {}'.format(str(member)))
+        if channel.name == "general":
+            await channel.send(channel, "Welcome {}".format(str(member)))
+
 
 client.run(token)
